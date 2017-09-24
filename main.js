fe3044673c0a18a26ef774bd08e761aae70f3639
newoc1 <owencapo1@gmail.com>
2017-09-24 17:30:02 -0400

    tabs now run away from each other if a collision is detected.

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ var tabObjects = []
 var fontDirectory = 'node_modules/three/examples/fonts/helvetiker_regular.typeface.json';
 
 function bootStrap() {
-	var camera, scene, renderer;	
+	var camera, scene, renderer;
 
 	init();
-	
+
 
 	function init() {
 		scene = new THREE.Scene();
@@ -20,21 +20,16 @@ function bootStrap() {
 			antialias: false
 		});
 		renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(renderer.domElement);		
+		document.body.appendChild(renderer.domElement);
 
 		function animate() {
 			requestAnimationFrame(animate);
-			// renderer.clear();
-			//TODO: there are better ways to do this using THREE.js apis and object ids
-			for(var i = 0; i < tabObjects.length; i++){
-				tabObjects[i].rotateY(0.05);
-				
-			}
+			separateTabs(tabObjects,0.7);
 			renderer.render(scene, camera);
 		}
 		camera.position.z = 350;
 		camera.position.y = 50
-			
+
 		chrome.windows.getCurrent({
 			populate: true
 		}, function(currentWindow) {
@@ -42,110 +37,92 @@ function bootStrap() {
 			var tabs = currentWindow.tabs;
 			for (var i = 0; i < tabs.length; i++) {
 				var tab = tabs[i];
-				createTab(tab, (textMesh) => {
-					textMesh.translateY(getRandomInt(-100, 200));
-					textMesh.rotateY(getRandomInt(0,2))
+				createTab(tab, (textMesh) => {					
 					tabObjects.push(textMesh)
 					scene.add(textMesh);
 				}, {
-				
+
 				});
 			}
-			animate();				
+
+			animate();
 		});
 	} // end init
 
 }
 
+// Uses bounding boxes to determine intersection with other tab objects.
+// If there is a collision it translates an arbitrary distance away from the tab object.
+// This method is meant to be called from a render loop (i.e. animate function)
+function separateTabs(tabs,moveDistance){
+
+	tabs.forEach(function(tabObj) {
+		var tabObjGeometry = tabObj.children[1].geometry;
+		var tabObjUUID = tabObj.uuid;				
+		var tabObjBoundingBox = new THREE.Box3().setFromObject(tabObj);				
+		for (var i = 0; i < tabs.length; i++) {					
+			if (tabObjUUID !== tabs[i].uuid) {						
+				var tabObjToCompareBoundingBox = new THREE.Box3().setFromObject(tabs[i]);
+				if (tabObjToCompareBoundingBox.intersectsBox(tabObjBoundingBox)) {								
+					var fromVector = tabObj.getWorldPosition();
+					var toVector = tabs[i].getWorldPosition();					
+					var dir = new THREE.Vector3();
+					dir.subVectors(toVector,fromVector).normalize();	
+					if(dir.x === 0 && dir.y === 0 && dir.z === 0){
+						dir = new THREE.Vector3(getRandomInt(0,100),getRandomInt(0,100), 0).normalize();
+					}							
+					tabs[i].translateOnAxis(dir, moveDistance);
+				}
+			}
+		}
+	});
+}
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+
 //generate a PlaneGeometry Mesh based off of tab properties (notabley title and url)
-function createTab(tab, callback,options){
+function createTab(tab, callback, options) {
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
-    ctx.font = '12pt verdana, sans-serif';
-    var textWidth = ctx.measureText(tab.title);
+	ctx.font = '16pt verdana, sans-serif';
+	var textWidth = ctx.measureText(tab.title);
 
-    canvas.width = textWidth.width
-    canvas.height = 16;
-    ctx.fillStyle = 'rgba(0,0,0,0.0)'
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#ffffff'
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(tab.title, canvas.width / 2, canvas.height / 2);
+	canvas.width = textWidth.width
+	canvas.height = 16;
+	ctx.fillStyle = 'rgba(0,0,0,0.0)'
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = '#ffffff'
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText(tab.title, canvas.width / 2, canvas.height / 2);
 
-	var texture =  new THREE.Texture(canvas);
+	var texture = new THREE.Texture(canvas);
 	texture.needsUpdate = true;
 
-	var material = new THREE.MeshBasicMaterial({map:texture,transparent:true, side: THREE.DoubleSide});
-	var geometry = new THREE.PlaneGeometry( canvas.width,canvas.height, 1 );	
-	var plane = new THREE.Mesh( geometry, material );
-	
-	callback(plane);
-}
-
-//WARNING: This probably won't be used. Justed keeping it around in case we want to have procedurally
-//generated 3d text in some cases
-//
-//Used to convert chrome tab data into a webgl object
-// The callback parameter is a function that takes a THREE Mesh object as a parameter
-//options include:
-// textGeometryOptions: see THREE.TextGeometry for more details. May also include a THREE Font object.
-// basicMaterialOptions: see THREE.MeshBasicMaterial for more details
-// font: A constructed THREE Font object usually provided by a FontLoader
-// context: The function context with which to call the callback
-// material: A configured THREE material to use instead of MeshBasicMaterial
-function create3DTab(tab, callback, options) {
-
-	var fontPromise;
-	if (!options && !options.font) {
-		console.warn('You should really be passing a font in to the function.');
-		loader = new THREE.FontLoader();
-		loader.load(fontDirectory, (font) => fontPromise = Promise.resolve(font))
-	} else {
-		if (options.textGeometryOptions && options.textGeometryOptions.font) {
-			font = options.textGeometryOptions.font
-		} else {
-			font = options.font;
-		}
-		fontPromise = Promise.resolve(font);
-	}
-	fontPromise.then((font) => {
-		//set up MeshBasicMaterial to use for the TextGeometry
-		if (options.material) {
-			var textMaterial = options.material;
-		} else if (options.basicMaterialOptions) {
-			var textMaterial = new THREE.MeshBasicMaterial(options.basicMaterialOptions);
-		} else {
-			var textMaterial = new THREE.MeshBasicMaterial({
-				color: 0xffffff
-			});
-		}
-		//Set up the TextGeometry
-		if (options.textGeometryOptions) {
-			var textGeometry = new THREE.TextGeometry(tab.title, options.textGeometryOptions);
-		} else {
-			var textGeometry = new THREE.TextGeometry(tab.title, {
-				font: font,
-				size: 1,
-				height: 0
-			})
-		}
-		var textMesh = new THREE.Mesh(textGeometry, textMaterial);
-
-		if (options.context) {
-			callback.call(options.context, textMesh);
-		} else {
-			callback(textMesh)
-		}
+	var material = new THREE.MeshBasicMaterial({
+		map: texture,
+		transparent: true,
+		side: THREE.DoubleSide
 	});
+	var geometry = new THREE.PlaneGeometry(canvas.width, canvas.height);
 
+	var plane = new THREE.Mesh(geometry, material);
 
+	var boundingBoxGeometry = new THREE.BoxGeometry(canvas.width, canvas.height, 2);
+	var boundingBoxMaterial = new THREE.MeshBasicMaterial()
+	boundingBoxMaterial.visible = false;
+	var boxMesh = new THREE.Mesh(boundingBoxGeometry, boundingBoxMaterial);
+	var tab = new THREE.Group();
+	tab.add(plane);
+	tab.add(boxMesh);
+
+	callback(tab);
 }
+
 document.addEventListener('DOMContentLoaded', function() {
 	bootStrap();
 });
